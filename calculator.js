9f8eed7e14fea05a3e366d48446c2bab6f47c434
SHEHROZ-BIBI5 <shehrozbibi5@gmail.com>
2025-04-27 09:51:48 +0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -4,22 +4,18 @@ const buttons = document.querySelectorAll("button"); // All buttons
 buttons.forEach((item) => {
     item.onclick = () => {
         if (item.dataset.button === 'C') {
-            // "C" دبانے پر اسکرین خالی
-            display.value = '';
+             display.value = '';
         } else if (item.dataset.button === 'CE') {
-            // "CE" دبانے پر آخری نمبر ہٹائیں
-            let string = display.value;
+             let string = display.value;
             display.value = string.substr(0, string.length - 1);
         } else if (item.dataset.button === '=') {
-            // "=" دبانے پر حساب لگائیں
-            try {
+             try {
                 display.value = eval(display.value); 
             } catch (e) {
                 display.value = "Error"; 
             }
         } else {
-            // باقی سب بٹن اسکرین میں شامل کریں
-            display.value += item.dataset.button;
+             display.value += item.dataset.button;
         }
     };
 });
